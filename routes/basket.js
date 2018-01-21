7621feaef6e7b78b3cb15a07d9888ac52b1b2d11
Kamil Szubiński <szubinskik@gmail.com>
2018-01-21 16:20:21 +0100

    More consitent basket AJAX

--- routes/basket.js
+++ routes/basket.js
@@ -67,60 +67,93 @@ module.exports = function(app){
   });
 
   // ajax basket
-  app.get('/_basket', function(req, res) {
+  app.put('/_basket', function(req, res) {
     
-    function _verify_id()
+    function handler(is_valid, nid)
     {
-      var Game = app.locals.Game;
-      var nid = parseInt(id);
-      if (isNaN(nid))
-        return false;
+      if (!is_valid)
+        res.end("1");
+      
+      if (false)
+      //if (!req.session.user)
+      {
+        res.render('basket/loggedoutBasket.ejs');
+        return;
+      }
 
-      Game.findAll({
-        where : { id : nid }
-      
-      }).then(ids => {
-        if(!ids || ids.length == 0)
-          return false;
-      
-      }).catch(err => {
-        console.error(err);
-        return false;
-      
-      });
+      if (!req.session.basket)
+        req.session.basket = _basket_session(); 
+      var basket = req.session.basket;
 
-      return true;
+      if (basket.items[nid])
+        basket.items[nid]++;
+      else
+        basket.items[nid] = 1;
+      
+      res.end('0');
     };
 
     var id = req.query.id;
-    if (!_verify_id())
-      res.end("1");
+    _verify_id(id, handler);
 
-    if (false)
-    //if (!req.session.user)
-    {
-      res.render('basket/loggedoutBasket.ejs');
-      return;
-    }
-
-    if (!req.session.basket)
-      req.session.basket = _basket_session(); 
-    var basket = req.session.basket;
-
-    if (basket.items[id])
-      basket.items[id]++;
-    else
-    {
-      basket.items[id] = 1;
-    }
-    res.end('0');
   });
 
+  app.delete('/_basket', function(req, res) {
+    
+    function handler(is_valid, nid)
+    {
+      if (false)
+      //if (!req.session.user)
+      {
+        res.end('1');
+        return;
+      }
+
+      if (!req.session.basket)
+        req.session.basket = _basket_session(); 
+      var basket = req.session.basket;
+
+      if (basket.items[nid])
+        basket.items[nid]--;
+
+      res.end('0');
+    }
+
+    var id = req.query.id;
+    _verify_id(id, handler);
+
+  });
+
+  // additional functions
+  // verify id and pass result (true - OK, flase - wrong) to handler
+  function _verify_id(id, handler)
+  {
+    var Game = app.locals.Game;
+    var nid = parseInt(id);
+    if (isNaN(nid))
+        return handler(false, NaN);
+
+    Game.findAll({
+      where : { id : nid }
+      
+    }).then(ids => {
+      if(!ids || ids.length == 0)
+        return handler(false, NaN);
+      else
+        return handler(true, nid);
+      
+    }).catch(err => {
+      console.error(err);
+      return handler(false, NaN);
+    });
+  };
+
+  function _basket_session()
+  {
+    return {
+      items : {}
+    }
+  }
+
 }
 
-function _basket_session()
-{
-  return {
-    items : {}
-  }
-}
