e88685c780bc29231603b142a71d8a5c4d305b2a
Michał Jaworski <jawor403@gmail.com>
2018-01-22 13:02:12 +0100

    Improve game validation

--- public/scripts/gameValidate.js
+++ public/scripts/gameValidate.js
@@ -1,32 +1,53 @@
 window.addEventListener("load", event => {
-    // Form validation
-    document.getElementById("gameForm").addEventListener("submit", event => {
-        event.preventDefault()
+    var form = document.getElementById("gameForm")
 
-        var title = document.getElementById("title").value
-        var price = document.getElementById("price").value
-                
-        var titleOk = false
-        var priceOk = false
+    var titleInput = document.getElementById("title")
+    var priceInput = document.getElementById("price")
+
+    var titleOutput = document.getElementById("titleMes")
+    var priceOutput = document.getElementById("priceMes")
+
+    // Title validation
+    function titleCheck() {
+        var title = titleInput.value
 
         if(title.length > 0 && title.length <= 100) {
-            document.getElementById("titleMes").innerHTML = ""
-            titleOk = true
+            titleOutput.innerHTML = ""
+            return true
         }
         else {
-            document.getElementById("titleMes").innerHTML = "Tytuł gry musi mieć od 1 do 100 znaków"
+            titleOutput.innerHTML = "Tytuł gry musi mieć od 1 do 100 znaków"
+            return false
         }
+    }
 
-        if(price.match(/^[1-9]\d*([\.,]\d\d)?$/)) {
-            document.getElementById("priceMes").innerHTML = ""
-            priceOk = true
+    // Price validation
+    function priceCheck() {
+        var price = priceInput.value
+
+        if(price.match(/^[1-9]\d*([\.,]\d\d?)?$/)) {
+            priceOutput.innerHTML = ""
+            return true
         }
         else {
-            document.getElementById("priceMes").innerHTML = "Niepoprawna cena"
+            priceOutput.innerHTML = "Niepoprawna cena"
+            return false
         }
+    }
 
-        if(titleOk && priceOk) {
-            document.getElementById("gameForm").submit()
+    // Input events
+    titleInput.addEventListener("change", event => {
+        titleCheck()
+    })
+
+    priceInput.addEventListener("change", event => {
+        priceCheck()
+    })
+
+    // Form validation
+    document.getElementById("gameForm").addEventListener("submit", event => {
+        if(!titleCheck() || !priceCheck()) {
+            event.preventDefault()
         }
     })
 })
