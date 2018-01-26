25f7401fa5e55f1aebaeffe035db21a23fdf97a9
Kamil Szubiński <szubinskik@gmail.com>
2018-01-26 22:53:12 +0100

    Remove unused routes, move main page to "/" route

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -1,7 +1,14 @@
+window.addEventListener('load', refresh_games(''));
+
 function b_search_clicked()
 {
-    var clist = document.getElementById('column_list');
     var phrase = document.getElementById('t_search').value;
+    refresh_games(phrase);    
+}
+
+function refresh_games(phrase)
+{
+    var clist = document.getElementById('column_list');
 
     var req = new XMLHttpRequest();
     req.open('get', `/_list?title=${phrase}`, true);
